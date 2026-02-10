8ca6216ad9d24a2b6c2fe49acd9580b3fa5d2d30
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2026-02-10 12:31:12 -0600

    Created two components in frame using Instance Principle

--- Frame/EmptyFrameViewer.java
+++ Frame/EmptyFrameViewer.java
@@ -1,28 +1,11 @@
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JButton;
 
 public class EmptyFrameViewer {
     public static void main(String[] args){
-        JFrame frame = new JFrame();
+        // Create a frame instance of FillJavaFrame class
+        JFrame frame = new FillJavaFrame();
 
-        // no visual apperanace
-        JButton button = new JButton("Click Me!");
-        JLabel label = new JLabel("Hello World!");
-
-        // create a panel object, and add button and label in panel
-        JPanel panel = new JPanel();
-        panel.add(button);
-        panel.add(label);
-
-        // add panel in frame to get the visual appearance
-        frame.add(panel);
-
-        final int FrameWidth = 300;
-        final int FrameHeight = 400;
-        frame.setSize(FrameHeight, FrameWidth);
-        frame.setTitle("Frame");
+        frame.setTitle("A Frame with two components");
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
---
+++ Frame/FillJavaFrame.java
@@ -0,0 +1,30 @@
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class FillJavaFrame extends JFrame {
+    // components as a instance variables
+    public JButton button;
+    public JLabel label;
+
+    public static final int FrameWidth = 300;
+    public static final int FrameHeight = 100;
+
+    // constructor
+    public FillJavaFrame() {
+        createComponent();
+        setSize(FrameWidth, FrameHeight);
+    }
+
+    public void createComponent() {
+        button = new JButton("Click Me!!!");
+        label = new JLabel("Keep on Smiling 😁");
+
+        JPanel panel = new JPanel();
+        panel.add(button);
+        panel.add(label);
+        add(panel);
+    }
+
+}
